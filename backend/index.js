c06278c93e47011f785550b49c956efe37202971
Abu Raihan <aburaihancse066@gmail.com>
2025-10-25 17:51:04 +0600

    read and add api done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,44 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "your_password",
+  database: "test",
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json({
+      status: 200,
+      message: "Book has been created successfully",
+      title: req.body.title,
+      desc: req.body.desc,
+      cover: req.body.cover,
+    });
+  });
+});
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800");
